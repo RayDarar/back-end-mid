ba190bdda023ac3e2523a0c735dc911bb98b5849
RayDarar <dfqgth400@gmail.com>
2020-03-24 05:10:43 +0600

    home-page suggestions implemented

---
+++ app/src/views/HomePage/HomePageSuggestions/SugItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="sug-item">
+    <img :src="getImage(image)" :alt="fullname" />
+    <span>{{ fullname }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SugItem",
+  props: {
+    fullname: String,
+    image: String
+  },
+  methods: {
+    getImage(image) {
+      const images = require.context("@/assets/", false, /\.png$/);
+      return images("./" + image);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sug-item {
+  margin: 0.5em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 80%;
+
+  span {
+    margin: 1em;
+    font-size: 1.2rem;
+  }
+
+  cursor: pointer;
+}
+</style>
--- app/src/views/HomePage/HomePageSuggestions/index.vue
+++ app/src/views/HomePage/HomePageSuggestions/index.vue
@@ -1,17 +1,47 @@
 <template>
-  <section class="suggestions"></section>
+  <section class="suggestions" @click="alertBox">
+    <alert-box ref="box" :time="1500"></alert-box>
+    <h2 class="title">People you may know</h2>
+    <sug-item fullname="Alina Danaeva" image="alina.png"></sug-item>
+  </section>
 </template>
 
 <script>
+import SugItem from "./SugItem";
+import AlertBox from "@/components/AlertBox";
+
 export default {
-  name: "HomePageSuggestions"
+  name: "HomePageSuggestions",
+  components: {
+    SugItem,
+    AlertBox
+  },
+  methods: {
+    alertBox() {
+      this.$refs.box.alert("Sorry, still in development");
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../../scss/_globals.scss";
+
 .suggestions {
   background-color: white;
   width: 100%;
   height: 100%;
+  border-radius: 10px;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    margin: 1em;
+    font-size: 1.1rem;
+    color: $grayText;
+  }
 }
 </style>
