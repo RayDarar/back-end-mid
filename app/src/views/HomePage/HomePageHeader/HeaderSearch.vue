988f3958e9556abe130834bc9d7b795bea3986ac
RayDarar <dfqgth400@gmail.com>
2020-03-24 03:27:49 +0600

    header search implemented

--- app/src/views/HomePage/HomePageHeader/HeaderSearch.vue
+++ app/src/views/HomePage/HomePageHeader/HeaderSearch.vue
@@ -1,11 +1,29 @@
 <template>
-  <div></div>
+  <div class="search">    
+    <input type="text" placeholder="Search" class="input-field" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HeaderSearch"
+  name: "HeaderSearch",
+  components: {}
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../../scss/_globals.scss";
+@import "../../../scss/_base.scss";
+
+.search {
+  height: 50%;
+  width: 30%;
+}
+.input-field {
+  padding: 0.3em 0.5em;
+  padding-left: 1em;
+  border: 5px solid $accentBlue;
+  border-radius: 15px;
+  width: 100%;
+}
+</style>
