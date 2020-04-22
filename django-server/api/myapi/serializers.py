c998ecee990b9a87a11e148e4aa5367532278f94
RayDarar <dfqgth400@gmail.com>
2020-04-22 14:29:06 +0600

    rewriting service

--- django-server/api/myapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = User
-    fields = ('id', 'first_name', 'last_name', 'gender', 'password', 'birthday', 'phone_number')
--- django-server/api/myapi/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from .serializers import UserSerializer
-from .models import User
-
-class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all().order_by('id')
-  serializer_class = UserSerializer
-  
